0da352b6cc19aa89f56a63b6735bf18fdf2aeee5
Taran09 <85576217+Taran67@users.noreply.github.com>
2021-08-23 17:41:45 +0100

    Add files via upload

---
+++ js/Boat.js
@@ -0,0 +1,52 @@
+class Boat {
+  constructor(x, y, width, height, boatPos, boat_animation) {
+    var options = {
+      restitution: 0.8,
+      friction: 1.0,
+      density: 1.0,
+      label: "boat"
+    };
+    
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    this.speed=0.05;
+    this.boatPosition = boatPos;
+    this.animation = boat_animation;
+    this.image = loadImage("assets/boat.png");
+    World.add(world, this.body);
+  }
+ 
+  animate(){
+    this.speed+=0.05%1.1
+  }
+
+  remove(index) {
+      this.animation=broken_boat_animation;
+      this.speed=0.05;
+      this.width = 300;
+      this.height = 200;
+
+     setTimeout(()=>{
+      Matter.World.remove(world, boats[index].body);
+      boats.splice(index, 1);
+     },2000)
+
+  
+  }
+
+  display() {
+    var angle = this.body.angle;
+    var pos = this.body.position;
+    var index = floor(this.speed%this.animation.length)
+   
+
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    imageMode(CENTER);
+    image(this.animation[index], 0, this.boatPosition, this.width, this.height);
+    noTint();
+    pop();
+  }
+}
